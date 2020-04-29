9e4ead1dfdbbbd362cbb8c3bea1916dbcfdfab23
mansichaudhary21 <49548815+mansichaudhary21@users.noreply.github.com>
2020-04-29 12:30:11 +0530

    Delete TicketDAOInterface.java

--- TrainManagement/src/main/java/com/dao/TicketDAOInterface.java
+++
@@ -1,9 +0,0 @@
-package com.dao;
-
-public interface TicketDAOInterface {
-
-	public void login(String unm,String psw) throws Exception;
-	public void showTrains() throws Exception;
-	public void bookTicket(int trainNo) throws Exception;
-	public void cancelTicket(int ticketNo) throws Exception;
-}
